620d710de014e42f4245d955a3fce21a65857efb
Vashar <vishal.pilani@live.com>
2019-03-12 17:57:15 +0530

    Add GET users/me and authentication middlewares

--- server/models/user.js
+++ server/models/user.js
@@ -37,7 +37,7 @@ UserSchema.methods.toJSON = function () {
     var user = this;
     var userObject = user.toObject();
 
-    return _.pick(userObject,['_id', 'email']);
+    return _.pick(userObject, ['_id', 'email']);
 }
 
 UserSchema.methods.generateAuthToken = function () {
@@ -52,6 +52,24 @@ UserSchema.methods.generateAuthToken = function () {
     })
 };
 
+UserSchema.statics.findByToken = function (token) {
+    var User = this;
+    var decoded = undefined;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+
+
+    }
+    catch (e) {
+        return Promise.reject();
+    }
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    })
+}
 var User = mongoose.model('User', UserSchema);
 
 module.exports = { User };
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { User } = require('./models/user');
 var { Todo } = require('./models/todo');
-
+var { authenticate } = require ('./middleware/authenticate')
 var app = express();
 const port = process.env.PORT;
 
@@ -114,24 +114,23 @@ app.patch('/todos/:id', (req, res) => {
 // });
 
 app.post('/users', (req, res) => {
-    
+
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(
         body
     );
 
 
-    user.save().then((user) => {
-        
-        return user.generateAuthToken();
+    user.save().then((user) => user.generateAuthToken())
+        .then((token) => {
+            res.header('x-auth', token).send(user);
+        }).catch((e) => {
+            res.status(400).send(e);
+        });
+});
 
-
-    }).then((token) => {
-
-        res.header('x-auth', token).send(user);
-    }).catch((e) => {
-        res.status(400).send(e);
-    });
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
 });
 
 app.listen(port, () => {
